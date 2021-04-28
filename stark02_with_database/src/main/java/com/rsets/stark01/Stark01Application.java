24cd644b27b7092055be5b8693b57c4bd293adc1
rsets <74058420+rsets@users.noreply.github.com>
2021-04-28 12:59:33 +0300

    Web Application Students

---
+++ stark02_with_database/src/main/java/com/rsets/stark01/Stark01Application.java
@@ -0,0 +1,13 @@
+package com.rsets.stark01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stark01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stark01Application.class, args);
+	}
+
+}
---
+++ stark02_with_database/src/test/java/com/rsets/stark01/Stark01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsets.stark01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stark01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
