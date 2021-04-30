4abbfb25b5627578520656ffa6061457168b38b5
rsets <74058420+rsets@users.noreply.github.com>
2021-04-30 16:13:35 +0300

    Delete stark02_with_database directory

--- stark02_with_database/src/main/java/com/rsets/stark01/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rsets.stark01.repo;
-
-import com.rsets.stark01.models.User;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
